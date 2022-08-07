05252db4b348cc769fad5a2e7334baa2ea1c6655
lordnosliam <jmailsons@gmail.com>
2022-08-07 17:39:22 -0300

    implementação dos testes vistos na aula

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,7 +5,7 @@ export class UserController {
   async createUser (request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body
 
-    if(name.length < 1 || email.length < 1) {
+    if(name?.length < 1 || email?.length < 1) {
       return response.status(400).json({})
     }
 
